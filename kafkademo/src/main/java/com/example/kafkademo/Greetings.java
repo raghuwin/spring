2c0739c7c02669ff91da8978b78ccc717e508f9d
raghuwin <35079384+raghuwin@users.noreply.github.com>
2018-04-12 15:07:50 -0400

    'message'

---
+++ kafkademo/src/main/java/com/example/kafkademo/Greetings.java
@@ -0,0 +1,15 @@
+package com.example.kafkademo;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@Builder
+public class Greetings {
+    private long timestamp;
+    private String message;
+}
